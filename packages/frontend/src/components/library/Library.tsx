46a33ecd3320dacbee1e11b1224f060caa690a9f
maayan mordehai <maayanmordehai3@gmail.com>
2022-03-26 20:43:07 +0300

    added icons

--- packages/frontend/src/components/library/Library.tsx
+++ packages/frontend/src/components/library/Library.tsx
@@ -1,13 +1,16 @@
-import { Grid, Box, Button, Stack, Typography } from '@mui/material';
+import { Grid, Box, Button, Stack, Typography, Divider } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { config } from '../../config/config';
 import { RootState } from '../../types/types';
 import CustomPaper from '../custom-paper/CustomPaper';
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+import ShortcutIcon from '@mui/icons-material/Shortcut';
+
 
 
 const Library = () => {
     const user = useSelector((state: RootState) => state.auth.user);
-
+    
     return (
         <div>
         <CustomPaper 
@@ -54,7 +57,53 @@ const Library = () => {
                             </Typography>
                         </Button>
                     </Grid>
+                </Grid>
+                <Grid container
+                    justifyContent="space-evenly"
+                    mt={2}
+                >
+                    <Grid item>
+                        <Button 
+                            variant="text"
+                        >
+                            <LibraryBooksIcon sx={{ fontSize: 60 }} color='action'/>
+                            <Typography fontWeight="bold" color="black" padding="5px">
+                                <Grid container>30</Grid>
+                                <Grid container>My Books</Grid>
+                            </Typography>
+                        </Button>
+                        <Divider />
                     </Grid>
+                    <Divider orientation="vertical" variant="middle" flexItem />
+                    <Grid item>
+                        <Button 
+                            variant="text"
+                        >                        
+
+                            <ShortcutIcon color='action'/>
+                            <LibraryBooksIcon sx={{ fontSize: 60 }} color='action'/>
+                            <Typography fontWeight="bold" color="black" padding="5px">
+                                <Grid container>10</Grid>
+                                <Grid container>Borrowed Books</Grid>
+                            </Typography>
+                        </Button>
+                        <Divider />
+                    </Grid>
+                    <Divider orientation="vertical" variant="middle" flexItem />
+                    <Grid item>
+                        <Button 
+                            variant="text"
+                        >
+                            <LibraryBooksIcon sx={{ fontSize: 60 }} color='action'/>
+                            <ShortcutIcon color='action'/>
+                            <Typography fontWeight="bold" color="black" padding="5px">
+                                <Grid container>20</Grid>
+                                <Grid container>Lent Books</Grid>
+                            </Typography>
+                        </Button>
+                        <Divider />
+                    </Grid>
+                </Grid>
             </Stack>
         </CustomPaper>
         </div>
