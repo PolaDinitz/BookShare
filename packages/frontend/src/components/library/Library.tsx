cdf0c80153a79ca6972843f4e30d12ed2a5a8609
Ran Biderman <bsuro10@gmail.com>
2022-04-28 00:45:32 +0300

    Reverted Library

--- packages/frontend/src/components/library/Library.tsx
+++ packages/frontend/src/components/library/Library.tsx
@@ -1,24 +1,36 @@
-import { Box, Button, Stack, Typography, Divider, List, ListItem } from '@mui/material';
-import { useSelector } from 'react-redux';
-import { config } from '../../config/config';
-import { RootState } from '../../types/types';
-import CustomPaper from '../common/custom-paper/CustomPaper';
-import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
-import ShortcutIcon from '@mui/icons-material/Shortcut';
-import React, { useState } from 'react';
-import BookCard from './cards/books-card/BookCard';
-import LibraryTab from './library-tab/LibraryTab';
-import TransactionCard from './cards/transaction-card/TransactionCard';
+import React, { useState } from "react";
+import { Box, Button, Divider, List, ListItem, Stack, Typography, } from "@mui/material";
+import { useSelector } from "react-redux";
+import LibraryBooksIcon from "@mui/icons-material/LibraryBooks";
+import ShortcutIcon from "@mui/icons-material/Shortcut";
+import CustomPaper from "../common/custom-paper/CustomPaper";
+import { config } from "../../config/config";
+import { RootState } from "../../types/types";
+import BookCard from "./cards/books-card/BookCard";
+import LibraryTab from "./library-tab/LibraryTab";
+import TransactionCard from "./cards/transaction-card/TransactionCard";
+import AddBook from "../home/AddBook";
 
 enum LibraryTabs {
     MY_BOOKS = "My Books",
     BORROWED_BOOKS = "Borrowed Books",
-    LENT_BOOKS = "Lent Books"
+    LENT_BOOKS = "Lent Books",
 }
 
 const Library = () => {
     const user = useSelector((state: RootState) => state.auth.user);
-    const [buttonSelected, changedButtonSelected] = useState(LibraryTabs.MY_BOOKS);
+    const [buttonSelected, changedButtonSelected] = useState(
+        LibraryTabs.MY_BOOKS
+    );
+
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     return (
         <CustomPaper
@@ -27,85 +39,140 @@ const Library = () => {
             size="large"
             avatarImg={`${config.apiUrl}/${user?.imageUrl}`}
         >
-            <Stack
-                spacing={3}
-                alignItems="center"
-                mb={2}
-            >
-                <Typography
-                    variant="h4"
-                    mt={2}
-                    fontWeight={600}
-                >
+            <Stack spacing={3} alignItems="center" mb={2}>
+                <Typography variant="h4" mt={2} fontWeight={600}>
                     {user?.firstName} {user?.lastName}'s Library
                 </Typography>
-                <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
-                    <Button
-                        sx={{ borderRadius: 10 }}
-                        variant="contained"
-                    >
+                <Box sx={{display: "flex", justifyContent: "flex-end"}}>
+                    <Button sx={{borderRadius: 10}} variant="contained" onClick={handleClickOpen}>
                         <Typography variant="subtitle2" fontWeight="bold">
                             + Add a Book to Library
                         </Typography>
                     </Button>
                 </Box>
-                <List sx={{ display: 'flex', flexDirection: 'row', padding: 5, width: "100%" }}>
+                <List
+                    sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        padding: 5,
+                        width: "100%",
+                    }}
+                >
                     <ListItem>
                         <LibraryTab
                             headline={LibraryTabs.MY_BOOKS.toString()}
                             amount={30}
                             selected={buttonSelected === LibraryTabs.MY_BOOKS}
-                            click={() => changedButtonSelected(LibraryTabs.MY_BOOKS)}>
-                            <LibraryBooksIcon sx={{ fontSize: 60 }} color='action' />
+                            click={() => changedButtonSelected(LibraryTabs.MY_BOOKS)}
+                        >
+                            <LibraryBooksIcon sx={{fontSize: 60}} color="action"/>
                         </LibraryTab>
                     </ListItem>
-                    <Divider orientation="vertical" variant="middle" flexItem />
+                    <Divider orientation="vertical" variant="middle" flexItem/>
                     <ListItem>
-                        <LibraryTab headline={LibraryTabs.BORROWED_BOOKS.toString()} amount={10}
+                        <LibraryTab
+                            headline={LibraryTabs.BORROWED_BOOKS.toString()}
+                            amount={10}
                             selected={buttonSelected === LibraryTabs.BORROWED_BOOKS}
                             click={() => changedButtonSelected(LibraryTabs.BORROWED_BOOKS)}
                         >
                             <>
-                                <ShortcutIcon color='action' />,
-                                <LibraryBooksIcon sx={{ fontSize: 60 }} color='action' />
+                                <ShortcutIcon color="action"/>,
+                                <LibraryBooksIcon sx={{fontSize: 60}} color="action"/>
                             </>
                         </LibraryTab>
                     </ListItem>
-                    <Divider orientation="vertical" variant="middle" flexItem />
+                    <Divider orientation="vertical" variant="middle" flexItem/>
                     <ListItem>
-                        <LibraryTab headline={LibraryTabs.LENT_BOOKS.toString()} amount={20}
+                        <LibraryTab
+                            headline={LibraryTabs.LENT_BOOKS.toString()}
+                            amount={20}
                             selected={buttonSelected === LibraryTabs.LENT_BOOKS}
-                            click={() => changedButtonSelected(LibraryTabs.LENT_BOOKS)}>
+                            click={() => changedButtonSelected(LibraryTabs.LENT_BOOKS)}
+                        >
                             <>
-                                <LibraryBooksIcon sx={{ fontSize: 60 }} color='action' />
-                                <ShortcutIcon color='action' />
+                                <LibraryBooksIcon sx={{fontSize: 60}} color="action"/>
+                                <ShortcutIcon color="action"/>
                             </>
                         </LibraryTab>
                     </ListItem>
                 </List>
-                {buttonSelected === LibraryTabs.MY_BOOKS &&
+                {buttonSelected === LibraryTabs.MY_BOOKS && (
                     <>
-                        <BookCard imageUrl='/page-headers/library-header-image.jpg' catagory='Fantacy' author='J.K. Rolling' name='Harry Potter' available={false} lent={false}/>
-                        <BookCard imageUrl='/page-headers/library-header-image.jpg' catagory='Fantacy' author='J.K. Rolling' name='Harry Potter' available={true} lent={true}/>
-                        <BookCard imageUrl='/page-headers/library-header-image.jpg' catagory='Fantacy' author='J.K. Rolling' name='Harry Potter' available={true} lent={false}/>
+                        <BookCard
+                            imageUrl="/page-headers/library-header-image.jpg"
+                            catagory="Fantacy"
+                            author="J.K. Rolling"
+                            name="Harry Potter"
+                            available={false}
+                            lent={false}
+                        ></BookCard>
+                        <BookCard
+                            imageUrl="/page-headers/library-header-image.jpg"
+                            catagory="Fantacy"
+                            author="J.K. Rolling"
+                            name="Harry Potter"
+                            available={true}
+                            lent={true}
+                        ></BookCard>
+                        <BookCard
+                            imageUrl="/page-headers/library-header-image.jpg"
+                            catagory="Fantacy"
+                            author="J.K. Rolling"
+                            name="Harry Potter"
+                            available={true}
+                            lent={false}
+                        ></BookCard>
                     </>
-                }
-                {buttonSelected === LibraryTabs.BORROWED_BOOKS &&
+                )}
+                {buttonSelected === LibraryTabs.BORROWED_BOOKS && (
                     <>
-                        <TransactionCard imageUrl='/page-headers/library-header-image.jpg' active={true} catagory='Fantacy' author='J.K. Rolling' name='Harry Potter' lentUserId="3639e574-e243-4443-ad5c-e682ede9598d" borrowedUserId='43230e94-6ae9-4b75-b092-73d94d6286b0' />
-                        <TransactionCard imageUrl='/page-headers/library-header-image.jpg' active={false} catagory='Fantacy' author='J.K. Rolling' name='Harry Potter' lentUserId="3639e574-e243-4443-ad5c-e682ede9598d" borrowedUserId='43230e94-6ae9-4b75-b092-73d94d6286b0' />
+                        <TransactionCard
+                            imageUrl="/page-headers/library-header-image.jpg"
+                            active={true}
+                            catagory="Fantacy"
+                            author="J.K. Rolling"
+                            name="Harry Potter"
+                            lentUserId="3639e574-e243-4443-ad5c-e682ede9598d"
+                            borrowedUserId="43230e94-6ae9-4b75-b092-73d94d6286b0"
+                        />
+                        <TransactionCard
+                            imageUrl="/page-headers/library-header-image.jpg"
+                            active={false}
+                            catagory="Fantacy"
+                            author="J.K. Rolling"
+                            name="Harry Potter"
+                            lentUserId="3639e574-e243-4443-ad5c-e682ede9598d"
+                            borrowedUserId="43230e94-6ae9-4b75-b092-73d94d6286b0"
+                        />
                     </>
-                }
-                {buttonSelected === LibraryTabs.LENT_BOOKS &&
+                )}
+                {buttonSelected === LibraryTabs.LENT_BOOKS && (
                     <>
-                        <TransactionCard imageUrl='/page-headers/library-header-image.jpg' active={true} catagory='Fantacy' author='J.K. Rolling' name='Harry Potter' lentUserId='43230e94-6ae9-4b75-b092-73d94d6286b0' borrowedUserId='3639e574-e243-4443-ad5c-e682ede9598d' />
-                        <TransactionCard imageUrl='/page-headers/library-header-image.jpg' active={false} catagory='Fantacy' author='J.K. Rolling' name='Harry Potter' lentUserId='43230e94-6ae9-4b75-b092-73d94d6286b0' borrowedUserId='3639e574-e243-4443-ad5c-e682ede9598d' />
+                        <TransactionCard
+                            imageUrl="/page-headers/library-header-image.jpg"
+                            active={true}
+                            catagory="Fantacy"
+                            author="J.K. Rolling"
+                            name="Harry Potter"
+                            lentUserId="43230e94-6ae9-4b75-b092-73d94d6286b0"
+                            borrowedUserId="3639e574-e243-4443-ad5c-e682ede9598d"
+                        />
+                        <TransactionCard
+                            imageUrl="/page-headers/library-header-image.jpg"
+                            active={false}
+                            catagory="Fantacy"
+                            author="J.K. Rolling"
+                            name="Harry Potter"
+                            lentUserId="43230e94-6ae9-4b75-b092-73d94d6286b0"
+                            borrowedUserId="3639e574-e243-4443-ad5c-e682ede9598d"
+                        />
                     </>
-                }
-
-            </Stack >
-        </CustomPaper >
-    )
-}
+                )}
+                <AddBook open={open} onClose={handleClose}/>
+            </Stack>
+        </CustomPaper>
+    );
+};
 
 export default Library;
