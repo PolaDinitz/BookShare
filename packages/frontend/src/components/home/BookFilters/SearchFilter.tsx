ee5ba1ca1e7b94e2f0b5ffcc40454813edef8f78
PolaDinitz <44160650+PolaDinitz@users.noreply.github.com>
2022-04-27 22:30:02 +0300

    search box styling

--- packages/frontend/src/components/home/BookFilters/SearchFilter.tsx
+++ packages/frontend/src/components/home/BookFilters/SearchFilter.tsx
@@ -12,27 +12,35 @@ const SearchFilter = (props: SearchFilterProps) => {
   const { searchText, onTextChange } = props;
 
   return (
-    <Paper
-      component="form"
-      elevation={2}
-      sx={{
-        p: "2px 4px",
-        display: "flex",
-        alignItems: "center",
+    <div
+      style={{
         width: "30%",
-        marginBottom: "20px",
-        marginRight: "10px",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
       }}
     >
       <InputBase
-        sx={{ ml: 1, flex: 1 }}
+        sx={{
+          ml: 1,
+          flex: 1,
+          width: "100%",
+          height: "2.8rem",
+          background: "#DADADA",
+          outline: "none",
+          border: "none",
+          borderRadius: "1.625rem",
+          padding: "0 3.5rem 0 1.5rem",
+          fontSize: "1rem",
+        }}
         placeholder="Search"
         onChange={onTextChange}
+        value={searchText}
       />
       <IconButton sx={{ p: "10px" }} aria-label="search">
         <SearchIcon />
       </IconButton>
-    </Paper>
+    </div>
   );
 };
 
