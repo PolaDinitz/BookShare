49e35f323b6d685973708da597b25d2e6976b50b
maayan mordehai <maayanmordehai3@gmail.com>
2022-05-11 17:17:59 +0300

    fixed module for userservice and bookservice

--- packages/backend/src/modules/transaction/transaction.module.ts
+++ packages/backend/src/modules/transaction/transaction.module.ts
@@ -4,9 +4,11 @@ import { TransactionController } from './transaction.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Transaction } from './entities/transaction.entity';
 import { UserBookModule } from 'src/modules/user-book/user-book.module';
+import { BookModule } from '../book/book.module';
+import { UsersModule } from '../user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transaction]), UserBookModule],
+  imports: [TypeOrmModule.forFeature([Transaction]), UserBookModule, BookModule, UsersModule],
   controllers: [TransactionController],
   providers: [TransactionService],
   exports: [TransactionService]
