7d0d6fef7f572b600af3f292b021795fe67d3a0a
Maayan Mordehai <maayanmordehai3@gmail.com>
2022-05-11 15:05:05 +0200

    change to functions (#54)

---
+++ packages/backend/src/transaction/transactionStatus.handlers.ts
@@ -0,0 +1,84 @@
+import { HttpException, HttpStatus } from "@nestjs/common";
+import { TransactionStatus } from "src/enums/transaction-status.enum";
+import { Transaction } from "./entities/transaction.entity";
+
+
+export function handleChangeStatusFromWaitingForChat(transaction : Transaction, currentUserId : string, newStatus : TransactionStatus) : boolean{
+  // In waiting for chat approval the borrow user can:
+  // - cancel the chat request
+  // The lend user can
+  // - decline chat
+  // - approve chat and update state to wating for lend
+  let isActive = true;
+  if (transaction.userBook.userId !== currentUserId) {
+    if (newStatus !== TransactionStatus.CHAT_CANCELED) {
+      throw new HttpException("You can't change to this state from current state", HttpStatus.BAD_REQUEST);
+    }
+    isActive = false;
+  }
+  else if (newStatus === TransactionStatus.CHAT_DECLINED) {
+    isActive = false;
+  }
+  else if (newStatus !== TransactionStatus.WAITING_FOR_LEND) {
+    throw new HttpException("You can't change to this state from current state", HttpStatus.BAD_REQUEST);
+  }
+  return isActive;
+}
+
+export function handleChangeStatusFromWaitingForLend(transaction : Transaction, currentUserId : string, newStatus: TransactionStatus) : boolean {
+    // In waiting for lent the borrow user can:
+    // - cancel the chat
+    // The lend user can
+    // - decline lent
+    // - approve lent and pass to waiting for book returnal status
+    let isActive = true;
+    if (transaction.userBook.userId !== currentUserId) {
+      if (newStatus !== TransactionStatus.CHAT_CANCELED) {
+        throw new HttpException("You can't change to this state from current state", HttpStatus.BAD_REQUEST);
+      }
+      isActive = false;
+    } else if (newStatus === TransactionStatus.LEND_DECLINED) {
+      isActive = false;
+    } else if (newStatus === TransactionStatus.WAITING_FOR_BOOK_RETURNED) {
+      this.userBookService.updateUserBookLent(transaction.userBookId, true);
+      this.transactionService.deactivateOtherTransactionsByUserBook(transaction.userBookId, transaction.id);
+    } else {
+      throw new HttpException("You can't change to this state from current state", HttpStatus.BAD_REQUEST);
+    }
+    return isActive;
+}
+
+export async function handleChangeStatusFromWaitingForBookReturn(transaction : Transaction, currentUserId : string, newStatus: TransactionStatus) : Promise<boolean> {
+  // in waiting for book returnal the borrow user can: 
+  // - return the book and change the status to waiting for book return approval
+  // The lent user can
+  // - say the book was returned and finish the transaction
+  let isActive = true;
+  if (transaction.userBook.userId !== currentUserId) {
+    if (newStatus !== TransactionStatus.WAITING_FOR_RETURN_APPROVAL) {
+      throw new HttpException("You can't change to this state from current state", HttpStatus.BAD_REQUEST);
+    }
+  } else if (newStatus === TransactionStatus.FINSHED_TRANSACTION) {
+    await this.userBookService.updateUserBookLent(transaction.userBookId, false);
+    isActive = false;
+  } else {
+    throw new HttpException("You can't change to this state from current state", HttpStatus.BAD_REQUEST);
+  }
+  return isActive;
+}
+
+export async function handleChangeStatusFromWaitingForReturnApproval(transaction : Transaction, currentUserId : string, newStatus: TransactionStatus) : Promise<boolean> {
+  // in waiting for book return approval the borrow user can't do anything.
+  // The lent user can
+  // - approve and finish the transaction
+  let isActive = true;    
+  if (transaction.userBook.userId !== currentUserId) {
+    throw new HttpException("UnAuthorized", HttpStatus.UNAUTHORIZED);
+  } else if (newStatus === TransactionStatus.FINSHED_TRANSACTION) {
+    await this.userBookService.updateUserBookLent(transaction.userBookId, false);
+    isActive = false;
+  } else {
+    throw new HttpException("You can't change to this state from current state", HttpStatus.BAD_REQUEST);
+  }
+  return isActive;
+}
