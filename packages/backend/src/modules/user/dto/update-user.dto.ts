846304ff3f078df059422b4bc3207d85a976125c
maayan mordehai <maayanmordehai3@gmail.com>
2022-06-01 19:01:01 +0300

    fixed update user

--- packages/backend/src/modules/user/dto/update-user.dto.ts
+++ packages/backend/src/modules/user/dto/update-user.dto.ts
@@ -1,4 +1,28 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { Type } from 'class-transformer';
+import { IsDate, IsEmail, IsEnum, IsPhoneNumber } from 'class-validator';
+import { Gender } from 'src/enums/gender.enum';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+    firstName: string;
+
+    lastName: string
+
+    @IsEmail()
+    email: string
+
+    password: string
+
+    confirmPassword: string
+
+    @IsEnum(Gender)
+    gender: Gender
+
+    @IsPhoneNumber("IL")
+    phoneNumber: string
+    
+    @IsDate()
+    @Type(() => Date)
+    dateOfBirth: Date
+    
+    address: string
+}
