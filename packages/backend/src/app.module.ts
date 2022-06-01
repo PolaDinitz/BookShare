b0fd5df50503395b78d639f3e0a912cacb629b52
maayan mordehai <maayanmordehai3@gmail.com>
2022-06-01 19:43:15 +0300

    spaces

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -22,18 +22,18 @@ import { ConfigModule, ConfigService } from "@nestjs/config";
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-			inject: [ConfigService],
+      inject: [ConfigService],
       useFactory: async (configService: ConfigService) => {
-				return {
-					type: 'postgres',
-					host: configService.get('DATABASE_HOST'),
-					port: configService.get<number>('DATABASE_PORT'),
-					username: configService.get('DATABASE_USER'),
-					password: configService.get('DATABASE_PASSWORD'),
-					database: String(configService.get('DATABASE_NAME')),
-					entities: ["dist/**/*.entity{.ts,.js}"],
-					synchronize: true,
-				};
+        return {
+          type: 'postgres',
+          host: configService.get('DATABASE_HOST'),
+          port: configService.get<number>('DATABASE_PORT'),
+          username: configService.get('DATABASE_USER'),
+          password: configService.get('DATABASE_PASSWORD'),
+          database: String(configService.get('DATABASE_NAME')),
+          entities: ["dist/**/*.entity{.ts,.js}"],
+          synchronize: true,
+        };
       }
     }),
     AuthModule,
