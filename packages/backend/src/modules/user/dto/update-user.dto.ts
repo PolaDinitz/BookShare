516b630e7709f3965ea235f6054ae14a8f6636aa
maayan mordehai <maayanmordehai3@gmail.com>
2022-06-03 16:17:07 +0300

    fixed

--- packages/backend/src/modules/user/dto/update-user.dto.ts
+++ packages/backend/src/modules/user/dto/update-user.dto.ts
@@ -1,28 +1,37 @@
 import { Type } from 'class-transformer';
-import { IsDate, IsEmail, IsEnum, IsPhoneNumber } from 'class-validator';
+import { IsDate, IsEmail, IsEnum, IsOptional, IsPhoneNumber } from 'class-validator';
 import { Gender } from 'src/enums/gender.enum';
 
 export class UpdateUserDto {
+    @IsOptional()
     firstName: string;
 
+    @IsOptional()
     lastName: string
 
+    @IsOptional()
     @IsEmail()
     email: string
 
+    @IsOptional()
     password: string
 
+    @IsOptional()
     confirmPassword: string
 
+    @IsOptional()
     @IsEnum(Gender)
     gender: Gender
 
+    @IsOptional()
     @IsPhoneNumber("IL")
     phoneNumber: string
     
+    @IsOptional()
     @IsDate()
     @Type(() => Date)
     dateOfBirth: Date
     
+    @IsOptional()
     address: string
 }
