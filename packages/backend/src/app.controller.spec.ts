05812125459060243753c1b5273b69b674badf3e
maayan mordehai <maayanmordehai3@gmail.com>
2022-06-03 19:17:27 +0300

    added auth testing

--- packages/backend/src/app.controller.spec.ts
+++ packages/backend/src/app.controller.spec.ts
@@ -1,25 +1,90 @@
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { Test, TestingModule } from '@nestjs/testing';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AuthModule } from './modules/authentication/auth.module';
-import { UsersModule } from './modules/user/user.module';
+import { UsersService } from './modules/user/user.service';
+import { AuthService } from './modules/authentication/auth.service'
+import { JwtStrategy } from './modules/authentication/jwt/jwt.strategy';
+import { LocalStrategy } from './modules/authentication/local/local.strategy';
+import { Role } from './enums/role.enum';
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
 
-describe('AppController', () => {
-  let appController: AppController;
+const moduleMocker = new ModuleMocker(global);
+
+const fakeUser = {
+  id: 1, 
+  password: "$2a$12$tbe9XOum3b3bDNZslBcB8e9h2nee8X/4ziPUPY9YczEIMoz0wwJlC",
+  email: "book@share.com",
+  role: Role.USER
+}
+
+describe('AuthService', () => {
+  let service: AuthService;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      imports: [AuthModule],
-      controllers: [AppController]
-    }).compile();
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [
+        PassportModule,
+        JwtModule.register({
+          signOptions: { expiresIn: '60s' },
+        })
+      ],
+      providers: [AuthService, LocalStrategy, JwtStrategy],
+    })
+    .useMocker((token) => {
+      if (token === UsersService) {
+        return { getUserByEmail: jest.fn().mockResolvedValue(fakeUser) };
+      }
+      if (typeof token === 'function') {
+        const mockMetadata = moduleMocker.getMetadata(token) as MockFunctionMetadata<any, any>;
+        const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+        return new Mock();
+      }
+    })
+    .compile();
 
-    appController = app.get<AppController>(AppController);
+    service = moduleRef.get<AuthService>(AuthService);
   });
 
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
 
-  describe('root', () => {
-    it('should fail to login', () => {
-      expect(appController.login({ email : "notreal@notexist.com", password: "notexist"})).toThrowError;
-    }) 
+describe('validateUser', () => {
+  let service: AuthService;
+
+  beforeEach(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [
+        PassportModule,
+        JwtModule.register({
+          signOptions: { expiresIn: '60s' },
+        }),
+      ],
+      providers: [AuthService, LocalStrategy, JwtStrategy],
+    })
+    .useMocker((token) => {
+      if (token === UsersService) {
+        return { getUserByEmail: jest.fn().mockResolvedValue(fakeUser) };
+      }
+      if (typeof token === 'function') {
+        const mockMetadata = moduleMocker.getMetadata(token) as MockFunctionMetadata<any, any>;
+        const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+        return new Mock();
+      }
+    })
+    .compile();
+
+    service = moduleRef.get<AuthService>(AuthService);
+  });
+
+  it('should return a user object when credentials are valid', async () => {
+    const res = await service.validateUser('book@share.com', 'Aa123456');
+    expect(res.id).toEqual(1);
+  });
+
+  it('should return null when credentials are invalid', async () => {
+    const res = await service.validateUser('xxx', 'xxx');
+    expect(res).toBeNull();
   });
 });
