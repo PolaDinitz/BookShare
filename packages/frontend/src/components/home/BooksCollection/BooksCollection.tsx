ff10f21756fda9d42ee9e6adefd4aad0d0c7bad8
Ran Biderman <36992518+bsuro10@users.noreply.github.com>
2022-06-04 22:59:23 +0300

    Css modify (#95)
    * Modified book details css
    Made grid to contain 5 books
    
    * Revert home grid to 3
    
    * Fixed console errors

--- packages/frontend/src/components/home/BooksCollection/BooksCollection.tsx
+++ packages/frontend/src/components/home/BooksCollection/BooksCollection.tsx
@@ -1,36 +1,32 @@
-import { Grid, Pagination, Stack } from "@mui/material";
-import _ from "lodash";
+import { Grid } from "@mui/material";
 
 import BookPost from "../BookPost";
 import { Book } from "../../../features/books/book.model";
 
 type BooksCollectionProps = {
-  books: Book[];
+    books: Book[];
 };
 
 const BooksCollection = (props: BooksCollectionProps) => {
-  const { books } = props;
+    const {books} = props;
 
-  return (
-    <>
-      <Grid container rowSpacing={3} columnSpacing={3}>
-        {!_.isEmpty(books) ? (
-          books.map((book) => (
-            <Grid key={book.id} item xs={3}>
-              <BookPost key={book.id} book={book} />
+    return (
+        <>
+            <Grid container rowSpacing={3} columnSpacing={3}>
+                {books.length > 0 ? (
+                    books.map((book) => (
+                        <Grid key={book.id} item xs={3}>
+                            <BookPost book={book}/>
+                        </Grid>
+                    ))
+                ) : (
+                    <h3 style={{margin: "30px"}}>
+                        OH NO! We couldn't find any books for you
+                    </h3>
+                )}
             </Grid>
-          ))
-        ) : (
-          <h3 style={{ margin: "30px" }}>
-            OH NO! We couldn't find any books for you
-          </h3>
-        )}
-      </Grid>
-      {/*<Stack spacing={2} display="flex" alignItems="center">
-        <Pagination count={10} />
-      </Stack>*/}
-    </>
-  );
+        </>
+    );
 };
 
 export default BooksCollection;
