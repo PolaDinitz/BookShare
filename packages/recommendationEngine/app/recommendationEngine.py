ed829f6f7271cc79244b33ab724e026c63fe55eb
Ran Biderman <36992518+bsuro10@users.noreply.github.com>
2022-06-07 00:30:15 +0300

    Revert "Revert "Optimize recommendations" (#102)" (#104)
    This reverts commit a508a4fb944f79b67b913f869d645fd4e6bb1a59.

--- packages/recommendationEngine/app/recommendationEngine.py
+++ packages/recommendationEngine/app/recommendationEngine.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from joblib import load
-from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics.pairwise import cosine_similarity, linear_kernel
 import requests
 
 
@@ -13,21 +13,22 @@ def predict(ds, book_id):
         read_book = ds.iloc[[index]]
     except IndexError as e:
         read_book = get_new_book(book_id)
-
     book_tfidf = tfidf_vec.transform(read_book['full_text'])
-    cos_similarity_tfidf = map(lambda x: cosine_similarity(book_tfidf, x), tfidf_rev)
+    cos_similarity_tfidf = map(lambda x: linear_kernel(book_tfidf, x), tfidf_rev)
     output = list(cos_similarity_tfidf)
     return output
 
 
 def get_recommendation(top, ds):
-    recommendation = pd.DataFrame(columns=['title', 'google_book_id'])
-    count = 0
+    # recommendation = pd.DataFrame(columns=['title', 'google_book_id'])
+    recommendation = list()
+    # count = 0
     for i in top:
-        recommendation.at[count, 'google_book_id'] = ds.iloc[i, 26]
-        recommendation.at[count, 'title'] = ds.iloc[i, 11]
-        count += 1
-    return recommendation['google_book_id'].tolist()
+        recommendation.append((ds.iloc[i, 26], ds.iloc[i, 11]))
+        # recommendation.at[count, 'google_book_id'] = ds.iloc[i, 26]
+        # recommendation.at[count, 'title'] = ds.iloc[i, 11]
+        # count += 1
+    return recommendation
 
 
 def recommend_books(book_id):
@@ -39,9 +40,10 @@ def recommend_books(book_id):
 
 
 def get_new_book(book_id):
+    print("entered new book")
     r = requests.get(f'https://www.googleapis.com/books/v1/volumes/{book_id}').json()['volumeInfo']
     full_text = r['publishedDate'] + r['title'] + r['language'] + \
-        ''.join(str(author) for author in r['authors']) + r['description']
+                ''.join(str(author) for author in r['authors']) + r['description']
 
     d = {'google_book_id': [book_id], 'full_text': [full_text]}
     return pd.DataFrame(data=d, dtype=object)
