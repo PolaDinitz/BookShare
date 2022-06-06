59b53fb6d056e50284000221be96f4e360e9d3c8
Ran Biderman <36992518+bsuro10@users.noreply.github.com>
2022-06-07 00:19:26 +0300

    Revert "Optimize recommendations"

--- packages/recommendationEngine/app/main.py
+++ packages/recommendationEngine/app/main.py
@@ -1,6 +1,3 @@
-import time
-from random import sample
-
 from fastapi import FastAPI
 from recommendationEngine import recommend_books
 
@@ -14,7 +11,6 @@ async def root():
 
 @app.get("/recommend/{book_id}")
 async def get_recommendation(book_id: str):
-    start = time.time()
     rcmd_dct = {}
     for book in book_id.split(','):
         recommendations = recommend_books(book)
@@ -24,8 +20,6 @@ async def get_recommendation(book_id: str):
             else:
                 rcmd_dct[recommendation] = 1
     rcmd_dct = dict(sorted(rcmd_dct.items(), key=lambda item: item[1]))
-    book_id, title = zip(*sample(list(rcmd_dct.keys()), 10))
-    res_dct = {'book_id': book_id, 'title': title}
-    print("Time taken to build response: %s seconds" % (time.time() - start))
-
+    print(rcmd_dct.keys)
+    res_dct = {'book_id': list(rcmd_dct.keys())[:10]}
     return res_dct
